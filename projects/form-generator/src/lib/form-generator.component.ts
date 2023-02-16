1efa7f1dd4e5332818b27b008d4e9e7963a112e5
Peyman <peymankhosravi.honor@gmail.com>
2023-02-16 18:53:06 +0330

    initial commit

---
+++ projects/form-generator/src/lib/form-generator.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-form-generator',
+  template: `
+    <p>
+      form-generator works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class FormGeneratorComponent {
+
+}
---
+++ projects/form-generator/src/lib/form-generator.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { FormGeneratorComponent } from './form-generator.component';
+
+
+
+@NgModule({
+  declarations: [
+    FormGeneratorComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    FormGeneratorComponent
+  ]
+})
+export class FormGeneratorModule { }
